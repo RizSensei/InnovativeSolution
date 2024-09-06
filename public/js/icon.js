dc444e289db287c564f148b7d3842ee4ea172915
RizSensei <maharzanrizen@gmail.com>
2024-09-06 12:36:59 +0545

    project setup along with images for sectors and necessary icon links

---
+++ public/js/icon.js
@@ -0,0 +1,22 @@
+let youtubeIcon = <i class="fa-brands fa-youtube"></i>;
+let facebookIcon = <i class="fa-brands fa-facebook-f"></i>;
+
+let mailRegularIcon = <i class="fa-regular fa-envelope"></i>;
+let mailSolidIcon = <i class="fa-solid fa-envelope"></i>;
+
+let locationPinIcon = <i class="fa-regular fa-location-dot"></i>;
+let clockIcon = <i class="fa-regular fa-clock"></i>;
+
+let phoneRegularIcon = <i class="fa-regular fa-phone"></i>;
+let phoneSolidIcon = <i class="fa-solid fa-phone"></i>;
+
+let researchIcon = <i class="fa-solid fa-magnifying-glass"></i>;
+let dataIcon = <i class="fa-solid fa-database"></i>;
+let gisIcon = <i class="fa-solid fa-earth-asia"></i>;
+let codeIcon = <i class="fa-solid fa-code"></i>;
+let systemIcon = <i class="fa-solid fa-gears"></i>;
+
+let downloadIcon = <i class="fa-solid fa-download"></i>;
+
+let quoteLeftIcon = <i class="fa-solid fa-quote-left"></i>;
+let quoteRightIcon = <i class="fa-solid fa-quote-right"></i>;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'blue': '#2A3F62',
+        'purple': '#664FA1',
+        'light-black': '#444444'
+      }
+    },
+  },
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
+}
